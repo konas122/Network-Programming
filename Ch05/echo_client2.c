0a33ad88f5341d4bd5c7044bcd96ba19ebc3ae44
konas122 <2407626470@qq.com>
2023-10-19 10:29:48 +0800

    add Ch05 and update Ch04 & .gitignore

--- Ch05/echo_client2.c
+++ Ch05/echo_client2.c
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]) {
     int sock;
     struct sockaddr_in serv_addr;
     char message[BUF_SIZE];
-    int str_len;
+    int str_len, recv_len, recv_cnt;
 
     if (argc != 3) {
         printf("Usage : %s <IP> <port>\n", argv[0]);
@@ -41,8 +41,15 @@ int main(int argc, char* argv[]) {
         if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
             break;
 
-        write(sock, message, BUF_SIZE - 1);
-        str_len = read(sock, message, BUF_SIZE - 1);
+        str_len = write(sock, message, BUF_SIZE - 1);
+
+        recv_len = 0;
+        while (recv_len < str_len) {
+            recv_cnt = read(sock, message, BUF_SIZE - 1);
+            if (recv_cnt == -1)
+                error_handling("read() error!");
+            recv_len += recv_cnt;
+        }
         message[str_len] = 0;
         printf("Message from server: %s", message);
     }
